46fc9ff640beada059d962812beb518917fa3502
khlooodali <khloudabc123@gmail.com>
2023-08-23 14:38:39 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_ninja/features/loginandsignup/loginview.dart';
 import 'package:food_ninja/features/loginandsignup/signupview.dart';
 
 import 'core/global/theme/lightmodetheme.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const SignUpView(),
+      child: const LoginView(),
     );
   }
 }
